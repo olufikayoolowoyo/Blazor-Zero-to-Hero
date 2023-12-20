656d7ddea696d1aefa5038c6b91a6b54de8618a5
olufikayoolowoyo <olufikayoolowoyo@gmail.com>
2023-12-19 22:11:57 -0500

    Using ComponentBase Class

---
+++ ZeroToHero.Client/Pages/Counter/Counter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ZeroToHero.Client.Pages.Counter
+{
+    public class CounterBase : ComponentBase
+    {
+        public int currentCount { get; set; } = 8;
+        public void IncrementCount()
+        {
+            currentCount++;
+        }
+
+    }
+}
