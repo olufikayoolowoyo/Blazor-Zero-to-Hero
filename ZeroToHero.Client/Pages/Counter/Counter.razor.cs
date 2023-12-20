ece2d21e1e41dc2b2d801a9ab86be60879fcaa05
olufikayoolowoyo <olufikayoolowoyo@gmail.com>
2023-12-19 21:56:47 -0500

    Using Partial files

---
+++ ZeroToHero.Client/Pages/Counter/Counter.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace ZeroToHero.Client.Pages.Counter
+{
+    public partial class Counter
+    {
+        private int currentCount = 5;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+
+    }
+}
